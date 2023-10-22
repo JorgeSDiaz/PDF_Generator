4b84dfaaca7848a61e2666205555b28d0f6571c6
Jorge Saenz <saenzdiazj0@gmail.com>
2023-10-21 23:32:53 -0500

    Add render_pdf, get_wkhtmltopdf_bin

---
+++ pdf.py
@@ -0,0 +1,13 @@
+import subprocess
+import jinja2
+import pdfkit
+
+def get_wkhtmltopdf_bin():
+    return subprocess.run(['which', 'wkhtmltopdf'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+
+def render_pdf(template: str, context: dict):
+    rendered_html = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(searchpath='templates')
+    ).get_template(template).render(context)
+    config = pdfkit.configuration(wkhtmltopdf=get_wkhtmltopdf_bin())
+    pdfkit.from_string(rendered_html, context["full_name"] + '.pdf', configuration=config)
