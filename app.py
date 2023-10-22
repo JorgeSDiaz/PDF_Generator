75ddb71beadf1f6103520e5a286c62b89bb3a7bb
Jorge Saenz <saenzdiazj0@gmail.com>
2023-10-21 23:58:40 -0500

    Add pdf endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pdf import render_pdf
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/", status_code=200)
 async def root():
     return {"message": "Works!"}
+
+@app.get("/pdf", status_code=200)
+async def pdf():
+    return FileResponse(render_pdf(), media_type="application/pdf")
