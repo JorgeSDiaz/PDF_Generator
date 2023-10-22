598334f669ade11097f85dab9b24ce6e8e6b923c
Jorge Saenz <saenzdiazj0@gmail.com>
2023-10-22 00:29:37 -0500

    Add Student model

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+
 from pdf import render_pdf
 
 app = FastAPI()
+
+class Student(BaseModel):
+    Full_Name: str
+    Date_of_Birth: str
+    City_of_Residence: str
+    Contact_Address: str
+    Phone_Number: str
+    Email: str
+    Area_of_Interest: str
+    University_Major: str
+    Semester: str
+    Skills: list
+    Knowledge: list
+    Soft_Skills: list
+    ID_Number: str
+    English_Level: str
+    University_Degree: dict
+    High_School_Degree: dict
+    Work_Experience: list
 
 
 @app.get("/", status_code=200)
@@ -9,5 +31,6 @@ async def root():
     return {"message": "Works!"}
 
 @app.get("/pdf", status_code=200)
-async def pdf():
-    return FileResponse(render_pdf(), media_type="application/pdf")
+async def pdf(context: Student):
+    render_pdf("template.html", context.dict())
+    return FileResponse('pdfs/' + context.dict()["Full_Name"] + '.pdf' , media_type="application/pdf")
